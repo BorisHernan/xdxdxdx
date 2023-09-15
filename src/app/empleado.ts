14752edb61fb294f2e5ccb56b7344debfface075
BorisHernan <89934200+BorisHernan@users.noreply.github.com>
2023-09-14 21:37:23 -0500

    xdddd

---
+++ src/app/empleado.ts
@@ -0,0 +1,15 @@
+export class Empleado {
+
+
+  id:number;
+  nombre:string;
+  apellido:string;
+  email:string;
+  name:string;
+  director:string;
+  telephone:string;
+  address:string;
+  department:string;
+  active:string;
+
+}
